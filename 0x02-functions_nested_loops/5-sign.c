864ec32eab0a902bb5c3108fae35f3f044c90a9f
feven <fevenbayu5@gmail.com>
2023-10-11 12:43:55 -0700

    r

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,35 +1,28 @@
 #include "main.h"
 
 
-	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
+/**
+ * print_sign - entry point
+ * @c: character
+ * Description: return 1 if c is in lower case
+ * Return: 1 or 0
+ */
+int print_sign(int c)
+{
+	if (c > 0)
 	{
-	    int r;
-
-
-	    r = print_sign(98);
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    r = print_sign(0);
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    r = print_sign(0xff);
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    r = print_sign(-1);
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(r + '0');
-	    _putchar('\n');
-	    return (0);
+		_putchar('+');
+			return (1);
 	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+			}
+	else if (c < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
