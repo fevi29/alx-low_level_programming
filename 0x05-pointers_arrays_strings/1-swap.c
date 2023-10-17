f047a9a30464cfd4101310c65b6e51d639226c5f
feven <fevenbayu5@gmail.com>
2023-10-17 13:40:52 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,15 @@
 #include "main.h"
 /**
- * _strlen - returns the length of a string
- * @s: string
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
 
