189b457a3c683fcab2aa25b0f8138b39708582a1
feven <fevenbayu5@gmail.com>
2023-10-27 13:31:09 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,56 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
-* main - prints the min number of coins to make change
-* for an amount of money
-* @argc: argument count
-* @argv: arguments
-* Return: 0
-*/
+ * main - function to print out the change that needs to given
+ * @argc: number of arguments to be passed
+ * @argv: number of variables to calculate
+ * Return: always 0 for sucess
+ */
+
 int main(int argc, char **argv)
 {
-int total, count;
-unsigned int i;
-char *p;
-int cents[] = {25, 10, 5, 2};
+	int total, change;
 
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
+	change = atoi(argv[1]);
 
-
-total = strtol(argv[1], &p, 10);
-count = 0;
-
-
-if (!*p)
-{
-while (total > 1)
-{
-for (i = 0; i < sizeof(cents[i]); i++)
-{
-   if (total >= cents[i])
-   {
-       count += total / cents[i];
-       total = total % cents[i];
-   }
-}
-}
-if (total == 1)
-count++;
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-
-
-printf("%d\n", count);
-return (0);
+	for (total = 0; change > 0; total++)
+	{
+		if (change - 25 >= 0)
+			change = change - 25;
+		else if (change - 10 >= 0)
+			change = change - 10;
+		else if (change - 5 >= 0)
+			change = change - 5;
+		else if (change - 2 >= 0)
+			change = change - 2;
+		else if (change - 1 >= 0)
+			change = change - 1;
+	}
+	printf("%d\n", total);
+	return (0);
 }
