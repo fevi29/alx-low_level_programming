63573e4397e983844c15e5251aedfb499333be07
feven <fevenbayu5@gmail.com>
2023-11-01 11:55:20 -0700

    r

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,36 +12,36 @@ void create_word(char **, char *, int, int, int);
 */
 char **strtow(char *str)
 {
-  int i, flag, len;
-  char **words;
+int i, flag, len;
+char **words;
 
-  if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
-     return (NULL);
+if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+return (NULL);
 
-  i = flag = len = 0;
-  while (str[i])
-  {
-     if (flag == 0 && str[i] != ' ')
+i = flag = len = 0;
+while (str[i])
+{
+if (flag == 0 && str[i] != ' ')
         flag = 1;
-     if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-     {
-        flag = 0;
-        len++;
-     }
-     i++;
-  }
+if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+{
+flag = 0;
+len++;
+}
+i++;
+}
 
-  len += flag == 1 ? 1 : 0;
-  if (len == 0)
-     return (NULL);
+len += flag == 1 ? 1 : 0;
+if (len == 0)
+return (NULL);
 
-  words = (char **)malloc(sizeof(char *) * (len + 1));
-  if (words == NULL)
-     return (NULL);
+words = (char **)malloc(sizeof(char *) * (len + 1));
+if (words == NULL)
+return (NULL);
 
-  util(words, str);
-  words[len] = NULL;
-  return (words);
+util(words, str);
+words[len] = NULL;
+return (words);
 }
 
 /**
