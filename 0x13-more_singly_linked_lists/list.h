f4c5a7cfec03778535311bd77a92aa994c50129d
feven <fevenbayu5@gmail.com>
2023-11-21 12:43:36 -0800

    lists.h

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,84 +1,35 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
 
-#ifndef _CB_LIST_H_
-#define _CB_LIST_H_
-#include<stddef.h>
-/*
- * reference : linux2.6 include/linux/list.h
- * reference : http://adrianhuang.blogspot.com/2007/10/linux-kernel-listhead.html
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for alx project
  */
-
-typedef struct cb_list
+typedef struct listint_s
 {
-    struct cb_list *next,*prev;
-}cb_list;
-   
-#define cb_list_head(_l) {&(_l),&(_l)}
+        int n;
+        struct listint_s *next;
+} listint_t;
 
-static inline cb_list_init(cb_list *list)
-{
-    list->next = list;
-    list->prev = list;
-}
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-static inline void cb_list_add_(cb_list *n,cb_list *prev,cb_list *next)
-{
-    next->prev = n;
-    n->next = next;
-    n->prev = prev;
-    prev->next = n;
-}
-
-static inline void cb_list_insert(cb_list *list,cb_list *node)
-{
-    cb_list_add_(node,list->prev,list);
-}
-
-static inline void cb_list_add(cb_list *list,cb_list *node)
-{
-    cb_list_add_(node,list,list->next);
-}
-
-static inline void cb_list_catnode(cb_list *prev,cb_list *next)
-{
-    prev->next = next;
-    next->prev = prev;
-}
-
-static inline void cb_list_del(cb_list *node)
-{
-    cb_list_catnode(node->prev,node->next);
-}
-
-#define cb_list_entry(_p,_t,_m) ((_t*)((char*)(_p)-(unsigned long)(&((_t*)0)->_m)))
-
-#define cb_list_push(_head,_node) cb_list_insert((_head).next,_node)
-#define cb_list_append(_head,_node) cb_list_insert(&(_head),_node)
-#define cb_list_isempty(_head) (&(_head) == _head.next)
-#define cb_list_islast(_head,_node) ((_node)->next == (_head))
-cb_list* cb_list_pop(cb_list head);
-#define cb_list_popt(_head,_t,_m) cb_list_entry(cb_list_pop(_head),_t,_m)
-
-#define cb_list_foreach(_head,_p) for(_p=(_head).next;_p!=&(_head);_p=_p->next)
-#define cb_list_rforeach(_head,_p) for(_p=(_head).prev;_p!=&(_head);_p=_p->prev)
-
-#define cb_list_foreachs(_head,_p,_n) for(_p=(_head).next,_n=_p->next;\
-                                          _p!=&(_head);\
-                                          _p=_n,_n=_p->next)
-#define cb_list_rforeachs(_head,_p,_n) for(_p=(_head).prev,_n=_p->prev;\
-                                           _p!=&(_head);\
-                                           _p=_n,_n=_p->prev)
-
-#define cb_list_eforeach(_head,_p,_m) for(_p=cb_list_entry((_head).next,typeof(*_p),_m);\
-                                          &_p->_m!=&(_head);\
-                                          _p=cb_list_entry(_p->_m.next,typeof(*_p),_m))
-
-#define cb_list_erforeach(_head,_p,_m) for(_p=cb_list_entry((_head).prev,typeof(*_p),_m);\
-                                          &_p->_m!=&(_head);\
-                                          _p=cb_list_entry(_p->_m.prev,typeof(*_p),_m))
-
-void cb_list_catlist(cb_list *p,cb_list *n);
-void cb_list_cutlist(cb_list *p,cb_list *n,cb_list *nhead);
-
-void cb_list_rrotate(cb_list *p);
-void cb_list_lrotate(cb_list *p);
 #endif
